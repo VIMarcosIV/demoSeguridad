c9b32eba10b452e5b632bee37d080876b143d975
marcos <msanchezm@iesch.org>
2023-02-10 21:21:09 +0100

    Initial commit

---
+++ src/main/java/org/iesch/demoSeguridad/Config/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package org.iesch.demoSeguridad.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("pepe")
+                .password("{noop}" + "0000")
+                .roles("USER")
+                .build());
+
+        manager.createUser(User.withUsername("root")
+                .password("{noop}" + "1234")
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+
+                .antMatchers(HttpMethod.DELETE)
+                .hasRole("ADMIN")
+
+                .antMatchers("/admin/**")
+                .hasAnyRole("ADMIN")
+
+                .antMatchers("/publico/**")
+                .hasAnyRole("ADMIN", "USER")
+
+                .antMatchers("/login/**")
+                .anonymous()
+
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+
+    }
+
+}
